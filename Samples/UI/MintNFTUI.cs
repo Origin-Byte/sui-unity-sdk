df93badfed5fad94b66b32a03e4aebc7dd54d6d0
Tamas Szucs <tamas@originbyte.io>
2022-09-20 11:37:40 +0200

    MintNFT sample

---
+++ Samples/UI/MintNFTUI.cs
@@ -0,0 +1,82 @@
+using Suinet.Rpc;
+using Suinet.Rpc.Types;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MintNFTUI : MonoBehaviour
+{
+    public Button MintNFTButton;
+    public TMP_InputField NFTNameInputField;
+    public TMP_InputField NFTDescriptionInputField;
+    public TMP_InputField NFTUrlInputField;
+    public TMP_InputField GasObjectIdInputField;
+
+    public Image NFTImage;
+
+    private async void Start()
+    {
+        // Default text
+        NFTNameInputField.text = "Origin Byte NFT";
+        NFTDescriptionInputField.text = "NFT minted using SuiUnitySDK by Origin Byte";
+        NFTUrlInputField.text = "https://avatars.githubusercontent.com/u/112119979";
+
+        MintNFTButton.onClick.AddListener(async () =>
+        {
+            var rpcClient = new UnityWebRequestRpcClient(SuiConstants.DEVNET_ENDPOINT);
+            var suiJsonRpcApi = new SuiJsonRpcApiClient(rpcClient);
+
+            var signer = SuiWallet.Instance.GetActiveAddress();
+            var packageObjectId = "0x2";
+            var module = "devnet_nft";
+            var function = "mint";
+            var typeArgs = System.Array.Empty<string>();
+            var args = new object[] { NFTNameInputField.text, NFTDescriptionInputField.text, NFTUrlInputField.text };
+            var gasObjectId = GasObjectIdInputField.text;
+            var rpcResult = await suiJsonRpcApi.MoveCallAsync(signer, packageObjectId, module, function, typeArgs, args, gasObjectId, 2000);
+
+            if (rpcResult.IsSuccess)
+            {
+                var keyPair = SuiWallet.Instance.GetActiveKeyPair();
+
+                var txBytes = rpcResult.Result.TxBytes;
+                var signature = keyPair.Sign(rpcResult.Result.TxBytes);
+                var pkBase64 = keyPair.PublicKeyBase64;
+
+                var txRpcResult = await suiJsonRpcApi.ExecuteTransactionAsync(txBytes, SuiSignatureScheme.ED25519, signature, pkBase64);
+                if (txRpcResult.IsSuccess)
+                {
+                    await LoadNFT(NFTUrlInputField.text);
+                }
+                else
+                {
+                    Debug.LogError("Something went wrong when executing the transaction: " + txRpcResult.ErrorMessage);
+                }
+            }
+            else
+            {
+                Debug.LogError("Something went wrong with the move call: " + rpcResult.ErrorMessage);
+            }
+        });
+    }
+
+    private async Task LoadNFT(string url)
+    {
+        using var req = new UnityWebRequest(url, "GET");
+
+        req.downloadHandler = new DownloadHandlerBuffer();
+        req.SendWebRequest();
+        while (!req.isDone)
+        {
+            await Task.Yield();
+        }
+        var data = req.downloadHandler.data;
+
+        var tex = new Texture2D(256, 256);
+        tex.LoadImage(data);
+        var sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(tex.width / 2, tex.height / 2));
+        NFTImage.sprite = sprite;
+    }
+}
