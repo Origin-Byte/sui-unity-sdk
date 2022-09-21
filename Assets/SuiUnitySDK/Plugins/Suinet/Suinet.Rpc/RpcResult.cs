d61d9be8b592d3fc848f700bf9d0419f36324d94
Tamas Szucs <tamas@originbyte.io>
2022-09-21 20:40:22 +0200

    fix setter visibility

--- Assets/SuiUnitySDK/Plugins/Suinet/Suinet.Rpc/RpcResult.cs
+++ Assets/SuiUnitySDK/Plugins/Suinet/Suinet.Rpc/RpcResult.cs
@@ -4,8 +4,8 @@
     {
         public bool IsSuccess { get; set; }
         public T Result { get; set; }
-        public string RawRpcRequest { get; internal set; }
-        public string RawRpcResponse { get; internal set; }
+        public string RawRpcRequest { get; set; }
+        public string RawRpcResponse { get; set; }
         public string ErrorMessage { get; set; }
     }
 }
