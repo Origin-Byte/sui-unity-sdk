01f3cfc0d7d31dcfea1f94752c75a81d2a44874c
Tamas Szucs <tamas@originbyte.io>
2022-11-22 11:13:43 +0100

    fix SuiEventId

--- Plugins/Suinet/Suinet.Rpc.Types/SuiEventId.cs
+++ Plugins/Suinet/Suinet.Rpc.Types/SuiEventId.cs
@@ -1,9 +1,13 @@
-﻿namespace Suinet.Rpc.Types
+﻿using Newtonsoft.Json;
+
+namespace Suinet.Rpc.Types
 {
     public class SuiEventId
     {
+        [JsonProperty("eventSeq")]
         public long EventSeq { get; set; }
 
+        [JsonProperty("txSeq")]
         public long TxSeq { get; set; }
     }
 }
