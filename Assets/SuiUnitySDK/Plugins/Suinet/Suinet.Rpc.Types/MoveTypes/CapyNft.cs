586de6262b835e97bfd9e34280e91e44558dd90a
Tamas Szucs <tamas@originbyte.io>
2023-01-13 10:20:32 +0100

    add CapyNft

---
+++ Assets/SuiUnitySDK/Plugins/Suinet/Suinet.Rpc.Types/MoveTypes/CapyNft.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Suinet.Rpc.Types.MoveTypes
+{
+    [MoveType("0x[a-f0-9]{40}::capy::Capy")]
+    public class CapyNft
+    {
+        public UID Id { get; set; }
+
+        public int Gen { get; set; }
+
+        [JsonProperty("item_count")]
+        public int ItemCount { get; set; }
+
+        public string Link { get; set; }
+        public string Url { get; set; }
+
+    }
+}
