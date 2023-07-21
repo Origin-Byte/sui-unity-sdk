7c7159aa5f95252ad83401f76edd4e45e4cc10ac
Tamas Szucs <tamas@originbyte.io>
2023-07-21 07:08:33 +0200

    wallets items

--- Samples/Scripts/UI/SuiPlay/SuiPlayWalletUIController.cs
+++ Samples/Scripts/UI/SuiPlay/SuiPlayWalletUIController.cs
@@ -1,20 +1,35 @@
+using System.Linq;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using Newtonsoft.Json;
+using Suinet.NftProtocol.Nft;
+using Suinet.Rpc.Types;
 using Suinet.SuiPlay.Requests;
 
 public class SuiPlayWalletUIController : MonoBehaviour
 {
     public Button actionButton;
-    public TMP_InputField ouput; // outputs can be copy-pasted
+    public TMP_InputField output; // outputs can be copy-pasted
+    public TMP_InputField coinBalance; 
 
     private void Start()
     {
         actionButton.onClick.AddListener(async () =>
         {
-            var result = await SuiPlay.Client.GetPlayerProfileAsync(SuiPlayConfig.GAME_ID);
-            ouput.text = JsonConvert.SerializeObject(result, Formatting.Indented);
+            var playerProfile = await SuiPlay.Client.GetPlayerProfileAsync(SuiPlayConfig.GAME_ID);
+
+            var walletAddress = playerProfile.Value.Wallets.First().Value.Address;
+            var coinBalanceResult = await SuiApi.Client.GetBalanceAsync(walletAddress, null);
+            coinBalance.text = coinBalanceResult.Result.TotalBalance.ToString();
+
+            // var nftsResult = await SuiApi.Client.GetObjectsOwnedByAddressAsync(walletAddress, new ArtNftParser(),
+            //     options: ObjectDataOptions.ShowAll());
+            var filter = ObjectDataFilterFactory.CreateMatchAllFilter(ObjectDataFilterFactory.CreateAddressOwnerFilter(walletAddress));
+            var ownedObjectsResult = await SuiApi.Client.GetOwnedObjectsAsync(walletAddress,
+                new ObjectResponseQuery() { Filter = filter }, null, null);
+
+            output.text = JsonConvert.SerializeObject(coinBalanceResult.Result, Formatting.Indented) + "\n" + JsonConvert.SerializeObject(ownedObjectsResult.Result, Formatting.Indented);
         });
     }
 }
