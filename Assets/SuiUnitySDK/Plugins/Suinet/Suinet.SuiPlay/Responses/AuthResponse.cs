cf09b3dece0e201573a8a1564cdccf09da2bf614
Tamas Szucs <tamas@originbyte.io>
2023-07-21 02:38:51 +0200

    login flow

--- Assets/SuiUnitySDK/Plugins/Suinet/Suinet.SuiPlay/Responses/AuthResponse.cs
+++ Assets/SuiUnitySDK/Plugins/Suinet/Suinet.SuiPlay/Responses/AuthResponse.cs
@@ -2,6 +2,8 @@
 {
     public class AuthResponse
     {
-        public string AccessToken { get; set; }
+        public string IdToken { get; set; }
+        public string RefreshToken { get; set; }
+        public string ExpiresIn { get; set; }
     }
 }
